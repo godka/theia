aa7bdc40722a5e63f8912c0ef659a77758bc31c5
godka <mythkast@hotmail.com>
2016-07-14 20:26:21 +0800

    增加遗孤filetrans

---
+++ TheiaP2P/FileTrans.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Theia.P2P
+{
+    public class FileTrans
+    {
+        public class Client : Basic.JsonBase
+        {
+            public string filename;
+            public int from;
+            public int to;
+            public Client(string _filename,int _from,int _to)
+            {
+                MsgType = 103;  //client 1问client2要东西
+                filename = _filename;
+                from = _from;
+                to = _to;
+            }
+        }
+
+        public class Server : Basic.JsonBase
+        {
+            public string filename;
+            public byte[] data;
+            public Server(string _filename,byte[] _data)
+            {
+                MsgType = 104;
+                filename = _filename;
+                data = _data;
+            }
+        }
+    }
+}
